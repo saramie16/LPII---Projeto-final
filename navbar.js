d8f2a8e67de917eda13b4cd6e2c42df23551aa5c
saramie16 <104026612+saramie16@users.noreply.github.com>
2022-11-21 15:17:37 -0300

    Add files via upload

---
+++ navbar.js
@@ -0,0 +1,10 @@
+
+        const submenu = document.querySelector('.menu')
+
+        const navbar = document.querySelector('.navbar')
+
+        const abrir_menu = () => {
+
+            submenu.classList.toggle('active')
+            navbar.classList.toggle('active')
+        }
